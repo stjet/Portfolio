fa35295cb88c358e1fbf3e850f06022b71419022
Jon Dough <49297268+jetstream0@users.noreply.github.com>
2021-10-20 00:18:29 +0000

    testimonials

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = Flask('Portfolio', static_url_path='', static_folder="static")
 @app.route('/')
 def index():
   return send_file('index.html')
-
+  
 @app.route('/<name>.css')
 def style(name):
   try:
